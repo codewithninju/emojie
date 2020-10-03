7b0ecdb6f4cd0d66ce3051bd1ae41aa7cf7c54b1
codewithninju <70657014+codewithninju@users.noreply.github.com>
2020-10-02 22:14:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function makeImageHappy(image) {
+  image.src =
+  "https://www.iconfinder.com/data/icons/streamline-emoji-1/48/013-smiling-face-with-heart-eyes-512.png";
+}
+
+function makeImageSad(image) {
+  image.src =
+  "https://www.iconfinder.com/data/icons/streamline-emoji-1/48/060-weary-face-1-512.png";
+}
